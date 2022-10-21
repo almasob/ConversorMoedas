1115034220f83334061409f92be2d41ecbc05041
Allan <al.masob99@gmail.com>
2022-10-21 15:47:12 -0300

    Projeto concluido!

--- ConversorJava/src/alura/challenge/execucao/Conversor.java
+++ ConversorJava/src/alura/challenge/execucao/Conversor.java
@@ -9,52 +9,75 @@ public class Conversor {
 	
 	public static void main(String[] args) {
 		
-		Object[] opcoes = {"Conversor Moedas", "Conversor Temperatura"};
-		String escolha = JOptionPane.showInputDialog(null, "Escolha uma opção","Menu",JOptionPane.PLAIN_MESSAGE,null,opcoes,opcoes[0]).toString();
-		
-		if(escolha == "Conversor Moedas") {
-			ConversorMoeda conversorM = new ConversorMoeda();
+		while(true) {
+			Object[] opcoes = {"Conversor Moedas", "Conversor Temperatura"};
+			String escolha = JOptionPane.showInputDialog(null, "Escolha uma opção","Menu",JOptionPane.PLAIN_MESSAGE,null,opcoes,opcoes[0]).toString();
 			
-			Object[] opcoesConvert = {"Real para dolar", "Dolar para Real"};
-			String tipoDeConversao = JOptionPane.showInputDialog(null, "Escolha uma conversão", "Conversao Moeda",JOptionPane.PLAIN_MESSAGE,null,opcoesConvert,"Escolha").toString();
-			
-			String valor =JOptionPane.showInputDialog(null,"Qual valor?");
-			Double valorConverter = Double.parseDouble(valor.replace(",", "."));
-			
-			switch(tipoDeConversao) {
-			case "Real para dolar":
-				conversorM.realDolar(valorConverter);
-				break;
-			case "Dolar para Real":
-				conversorM.dolarReal(valorConverter);
-				break;
-			
+			if(escolha == "Conversor Moedas") {
+				ConversorMoeda conversorM = new ConversorMoeda();
+				
+				Object[] opcoesConvert = {"Real para dolar", "Dolar para Real", "Real para Euro","Euro para Real"};
+				String tipoDeConversao = JOptionPane.showInputDialog(null, "Escolha uma conversão", "Conversao Moeda",JOptionPane.PLAIN_MESSAGE,null,opcoesConvert,"Escolha").toString();
+				
+				try {
+				String valor =JOptionPane.showInputDialog(null,"Qual valor?");
+				Double valorConverter = Double.parseDouble(valor.replace(",", "."));
+				
+				
+				switch(tipoDeConversao) {
+				case "Real para dolar":
+					conversorM.realDolar(valorConverter);
+					break;
+				case "Dolar para Real":
+					conversorM.dolarReal(valorConverter);
+					break;
+				case "Real para Euro":
+					conversorM.realEuro(valorConverter);
+					break;
+				case "Euro para Real":
+					conversorM.euroReal(valorConverter);
+					break;
+				
+				}
+				}	catch(Exception e) {
+					
+					e.printStackTrace();
+					JOptionPane.showMessageDialog(null,"Valor inválido! \n Use apenas numeros inteiros ou decimais!","Alerta!",JOptionPane.ERROR_MESSAGE);
+					
+				}
+				
 			}
+			
+			
+			if(escolha == "Conversor Temperatura") {
+				ConversorTemperatura conversorT = new ConversorTemperatura();
+				
+				String tipoTemperatura = JOptionPane.showInputDialog(null,"Escolha a conversão","Conversão Temperatura",JOptionPane.PLAIN_MESSAGE,null, new Object[]{"Graus Celsius para Fahrenheit","Graus Fahrenheit para Celsius"},"Escolha").toString();
+				
+				String temperaturaAtual = JOptionPane.showInputDialog(null,"Qual a temperatura atual?");
+				Double valorConverter = Double.parseDouble(temperaturaAtual.replace(",", "."));
+				
+				switch(tipoTemperatura) {
+				case "Graus Celsius para Fahrenheit":
+					conversorT.celFa(valorConverter);
+					break;
+				case  "Graus Fahrenheit para Celsius":
+					conversorT.faCel(valorConverter);
+					break;
+				}
+				
+				
 			
 		}
 		
-		if(escolha == "Conversor Temperatura") {
-			ConversorTemperatura conversorT = new ConversorTemperatura();
 			
-			String tipoTemperatura = JOptionPane.showInputDialog(null,"Escolha a conversão","Conversão Temperatura",JOptionPane.PLAIN_MESSAGE,null, new Object[]{"Graus Celsius para Fahrenheit","Graus Fahrenheit para Celsius"},"Escolha").toString();
-			
-			String temperaturaAtual = JOptionPane.showInputDialog(null,"Qual a temperatura atual?");
-			Double valorConverter = Double.parseDouble(temperaturaAtual.replace(",", "."));
-			
-			switch(tipoTemperatura) {
-			case "Graus Celsius para Fahrenheit":
-				conversorT.celFa(valorConverter);
-				break;
-			case  "Graus Fahrenheit para Celsius":
-				conversorT.faCel(valorConverter);
+			int sair = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Continuar?", JOptionPane.YES_NO_OPTION);
+			if(sair == 1) {
 				break;
 			}
 			
-		}
+		}	
 		
-		
+		JOptionPane.showMessageDialog(null, "Programa finalizado!", "Concluído!",JOptionPane.INFORMATION_MESSAGE);
 	}
-	
-	
-
 }
--- ConversorJava/src/alura/challenge/funcoes/ConversorMoeda.java
+++ ConversorJava/src/alura/challenge/funcoes/ConversorMoeda.java
@@ -7,6 +7,10 @@ import javax.swing.JOptionPane;
 public class ConversorMoeda {
 	private double dolar = 5.22;
 	private double real = 0.19;
+	private double euro = 5.09;
+	private double realE = 0.20;
+			
+			
 	DecimalFormat formato = new DecimalFormat("#.##");
 
 	public void realDolar(double valor) {
@@ -16,6 +20,16 @@ public class ConversorMoeda {
 	
 	public void dolarReal(double valor) {
 		double resultado = valor * dolar;
+		JOptionPane.showMessageDialog(null, "Você tem R$" + formato.format(resultado));
+	}
+	
+	public void euroReal(double valor) {
+		double resultado = valor * euro;
+		JOptionPane.showMessageDialog(null, "Você tem R$" + formato.format(resultado));
+	}
+	
+	public void realEuro(double valor) {
+		double resultado = valor * realE;
 		JOptionPane.showMessageDialog(null, "Você tem R$" + formato.format(resultado));
 	}
 }
